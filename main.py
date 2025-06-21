a9ca2da8f640ff904870b37d41877f584f1f57a5
Long Yao <l2yao@users.noreply.github.com>
2025-06-21 19:40:36 +0000

    add resource for getting menu

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from amtb!")
+from mcp.server.fastmcp import FastMCP
+import requests
 
+mcp = FastMCP("AMTB")
+
+@mcp.resource("amtb://menu/{scope}", name="Menu")
+def get_menu(scope: str = "amtb") -> str:
+    """get menu dynamically from amtb.tw based on scope
+       default scope will be amtb
+    """
+    response = requests.get(f'https://www.amtb.tw/v2/newcategory?scope={scope}')
+    menu = response.json()
+    return menu
+
+@mcp.resource("amtb://menu/{scope}/{amtb_id}", name="Sub Menu")
+def get_sub_menu(amtb_id: str, scope: str = "amtb") -> str:
+    response = requests.get(f'https://www.amtb.tw/v2/newcategory/{amtb_id}')
+    subitem_json = response.json()
+    return subitem_json
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport='stdio')
+    #print(get_sub_menu("48"))
+    #print(get_menu())
